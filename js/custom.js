34286317362631d95337bf5e088b9d331ce4addb
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2019-04-29 14:53:36 +0700

    updated

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$( document ).ready(function( $ ) {
+    $( '#view-hotel' ).sliderPro({
+        width: 960,
+        height: 500,
+        fade: true,
+        arrows: true,
+        buttons: false,
+        fullScreen: true,
+        shuffle: true,
+        smallSize: 500,
+        mediumSize: 1000,
+        largeSize: 3000,
+        thumbnailArrows: true,
+        autoplay: false
+    });
+});
--- js/main.js
+++ js/main.js
@@ -29,15 +29,3 @@ $(document).ready(function(){
     };
     date_input.datepicker(options);
 })
-
-$(document).ready(function(){
-    var date_input=$('input[name="date2"]'); //our date input has the name "date"
-    var container=$('.container form').length>0 ? $('.container form').parent() : "body";
-    var options={
-      format: 'mm/dd/yyyy',
-      container: container,
-      todayHighlight: true,
-      autoclose: true,
-    };
-    date_input.datepicker(options);
-})
