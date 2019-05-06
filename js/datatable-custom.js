f7017721565c542c7ebc8902090020edca15259e
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2019-05-06 11:36:22 +0700

    edit

--- js/datatable-custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#example').DataTable( {
-        "paging":   false,
-        // "ordering": false,
-        "info":     false,
-        "searching": false
-    } );
-} );
