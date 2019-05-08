64d00949ec6bf1b241b4308d93ca18029771f834
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2019-05-08 16:01:20 +0700

    add registrasi

---
+++ js/js-mobile.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var date_input=$('input[name="inputTanggalPergi"] , input[name="inputTanggalPulang"]'); //our date input has the name "date"
+    var container=$('.container form').length>0 ? $('.container form').parent() : "body";
+    var options={
+      format: 'mm/dd/yyyy',
+      container: container,
+      todayHighlight: true,
+      autoclose: true,
+    };
+    date_input.datepicker(options);
+})
