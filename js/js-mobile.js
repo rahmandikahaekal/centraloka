18fa782d63b20159d22a4c4c302741ebb80b9ef3
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2019-05-10 16:25:49 +0700

    updated detail payment for plane ticket

--- js/js-mobile.js
+++ js/js-mobile.js
@@ -1,3 +1,24 @@
+var clock;
+		
+$(document).ready(function() {
+    var clock;
+
+    clock = $('.clock').FlipClock({
+        clockFace: 'DailyCounter',
+        autoStart: false,
+        showDays: false,
+        callbacks: {
+            stop: function() {
+                $('.message').html('The clock has stopped!')
+            }
+        }
+    });
+            
+    clock.setTime(24000);
+    clock.setCountdown(true);
+    clock.start();
+
+});
 $(document).ready(function(){
     var date_input=$('input[name="inputTanggalPergi"] , input[name="inputTanggalPulang"]'); //our date input has the name "date"
     var container=$('.container form').length>0 ? $('.container form').parent() : "body";
@@ -8,4 +29,4 @@ $(document).ready(function(){
       autoclose: true,
     };
     date_input.datepicker(options);
-})
+});
