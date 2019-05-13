c29b832af04e1728d1a1ba21886c1a3630e5e728
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2019-05-13 10:03:46 +0700

    add cari hotel mobile

--- js/js-mobile.js
+++ js/js-mobile.js
@@ -19,14 +19,4 @@ $(document).ready(function() {
     clock.start();
 
 });
-$(document).ready(function(){
-    var date_input=$('input[name="inputTanggalPergi"] , input[name="inputTanggalPulang"]'); //our date input has the name "date"
-    var container=$('.container form').length>0 ? $('.container form').parent() : "body";
-    var options={
-      format: 'mm/dd/yyyy',
-      container: container,
-      todayHighlight: true,
-      autoclose: true,
-    };
-    date_input.datepicker(options);
-});
+
